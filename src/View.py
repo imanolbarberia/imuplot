6ee2e3da160b861d25b607f531363247f6b58252
Imanol Barberia <imanol.barberia@gmail.com>
2020-11-19 18:50:38 +0100

    add tabs and missing axes

--- src/View.py
+++ src/View.py
@@ -6,6 +6,22 @@ import pyqtgraph as pg
 import math
 
 
+class SensorAxisPlot:
+    def __init__(self, cnv: pg.GraphicsLayoutWidget, name, pen):
+        self.plot = cnv.addPlot(title=name)
+        self.line = self.plot.plot([0]*20, pen=pen, name=name)
+
+
+class SensorCanvas(pg.GraphicsLayoutWidget):
+    def __init__(self, basename):
+        super().__init__()
+        self.x = SensorAxisPlot(self, basename+"X", (255, 0, 0))
+        self.y = SensorAxisPlot(self, basename+"Y", (0, 255, 0))
+        self.nextRow()
+        self.z = SensorAxisPlot(self, basename+"Z", (0, 0, 255))
+        self.a = SensorAxisPlot(self, "|"+basename+"|", (255, 255, 0))
+
+
 class View(QtWidgets.QMainWindow):
     """
     View class
@@ -20,17 +36,12 @@ class View(QtWidgets.QMainWindow):
         self.ui = uic.loadUi("ui/wndmain.ui", self)
 
         # Load plot
-        self._canvas = pg.GraphicsLayoutWidget()
-        self.ui.layPlots.addWidget(self._canvas)
-        self._plot_accx = self._canvas.addPlot(title="AccX")
-        self._plot_accx_line = self._plot_accx.plot([0]*20, pen=(255, 0, 0), name="AccX")
-        self._plot_accy = self._canvas.addPlot(title="AccY")
-        self._plot_accy_line = self._plot_accy.plot([0]*20, pen=(0, 255, 0), name="AccY")
-        self._canvas.nextRow()
-        self._plot_accz = self._canvas.addPlot(title="AccZ")
-        self._plot_accz_line = self._plot_accz.plot([0]*20, pen=(0, 0, 255), name="AccZ")
-        self._plot_acc = self._canvas.addPlot(title="|Acc|")
-        self._plot_acc_line = self._plot_acc.plot([0]*20, pen=(255, 255, 0), name="|Acc|")
+        self._acc_canvas = SensorCanvas("Acc")
+        self.ui.layAccel.addWidget(self._acc_canvas)
+        self._gyro_canvas = SensorCanvas("Gyro")
+        self.ui.layGyro.addWidget(self._gyro_canvas)
+        self._mag_canvas = SensorCanvas("Mag")
+        self.ui.layMag.addWidget(self._mag_canvas)
 
         # Set model
         self._model = Model()
@@ -90,8 +101,30 @@ class View(QtWidgets.QMainWindow):
         accz_data = [el[2] for el in data_set]
         acc_data = [math.sqrt(el[0]**2+el[1]**2+el[2]**2) for el in data_set]
 
-        # plot the current data
-        self._plot_accx_line.setData(accx_data)
-        self._plot_accy_line.setData(accy_data)
-        self._plot_accz_line.setData(accz_data)
-        self._plot_acc_line.setData(acc_data)
+        gyrox_data = [el[3] for el in data_set]
+        gyroy_data = [el[4] for el in data_set]
+        gyroz_data = [el[5] for el in data_set]
+        gyro_data = [math.sqrt(el[3]**2+el[4]**2+el[5]**2) for el in data_set]
+
+        magx_data = [el[6] for el in data_set]
+        magy_data = [el[7] for el in data_set]
+        magz_data = [el[8] for el in data_set]
+        mag_data = [math.sqrt(el[6]**2+el[7]**2+el[8]**2) for el in data_set]
+
+        # plot current ACC data
+        self._acc_canvas.x.line.setData(gyrox_data)
+        self._acc_canvas.y.line.setData(accy_data)
+        self._acc_canvas.z.line.setData(accz_data)
+        self._acc_canvas.a.line.setData(acc_data)
+
+        # plot current GYRO data
+        self._gyro_canvas.x.line.setData(gyrox_data)
+        self._gyro_canvas.y.line.setData(gyroy_data)
+        self._gyro_canvas.z.line.setData(gyroz_data)
+        self._gyro_canvas.a.line.setData(gyro_data)
+
+        # plot current MAG data
+        self._mag_canvas.x.line.setData(magx_data)
+        self._mag_canvas.y.line.setData(magy_data)
+        self._mag_canvas.z.line.setData(magz_data)
+        self._mag_canvas.a.line.setData(mag_data)
